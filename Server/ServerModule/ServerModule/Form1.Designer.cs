382f09d0142e9d81370ea23ac21c2bfeb40290ed
Mehmet Kiraz <fymmk.1903@gmail.com>
2022-05-13 19:49:01 +0300

    Bune aq

--- Server/ServerModule/ServerModule/Form1.Designer.cs
+++ Server/ServerModule/ServerModule/Form1.Designer.cs
@@ -30,19 +30,21 @@ namespace ServerModule
         private void InitializeComponent()
         {
             this.portTextBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.labelPort = new System.Windows.Forms.Label();
+            this.serverConsole = new System.Windows.Forms.RichTextBox();
+            this.buttonListen = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // portTextBox
             // 
-            this.portTextBox.Location = new System.Drawing.Point(120, 72);
+            this.portTextBox.Location = new System.Drawing.Point(83, 28);
+            this.portTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.portTextBox.Name = "portTextBox";
-            this.portTextBox.Size = new System.Drawing.Size(216, 22);
+            this.portTextBox.Size = new System.Drawing.Size(163, 20);
             this.portTextBox.TabIndex = 0;
             this.portTextBox.TextChanged += new System.EventHandler(this.portTextBox_TextChanged);
             // 
-            // label1
+            // labelPort
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(34, 75);
@@ -51,26 +53,28 @@ namespace ServerModule
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
-            // label2
+            // buttonListen
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(599, 89);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.buttonListen.Location = new System.Drawing.Point(266, 18);
+            this.buttonListen.Name = "buttonListen";
+            this.buttonListen.Size = new System.Drawing.Size(121, 38);
+            this.buttonListen.TabIndex = 3;
+            this.buttonListen.Text = "Listen";
+            this.buttonListen.UseVisualStyleBackColor = true;
+            this.buttonListen.Click += new System.EventHandler(this.buttonListen_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.buttonListen);
+            this.Controls.Add(this.serverConsole);
+            this.Controls.Add(this.labelPort);
             this.Controls.Add(this.portTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,8 +83,9 @@ namespace ServerModule
         #endregion
 
         private System.Windows.Forms.TextBox portTextBox;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.RichTextBox serverConsole;
+        private System.Windows.Forms.Button buttonListen;
     }
 }
 
--- Server/ServerModule/ServerModule/Form1.cs
+++ Server/ServerModule/ServerModule/Form1.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,19 +16,135 @@ namespace ServerModule
 {
     public partial class Form1 : Form
     {
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        List<Socket> clientSockets = new List<Socket>();
+        bool terminate = false;
+        bool listening = false;
         public Form1()
         {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+       
+        private void buttonListen_Click(object sender, EventArgs e)
         {
+            int serverPort;
 
+            if (Int32.TryParse(portTextBox.Text, out serverPort))
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, serverPort);
+                serverSocket.Bind(endPoint);
+                serverSocket.Listen(3);
+
+                listening = true;
+                buttonListen.Enabled = false;
+
+                Thread acceptThread = new Thread(Accept);
+                acceptThread.Start();
+
+                serverConsole.AppendText("Started listening on port: " + serverPort + "\n");
+            }
+            else
+            {
+                serverConsole.AppendText("Please check port number \n");
+            }
         }
 
-        private void portTextBox_TextChanged(object sender, EventArgs e)
+        private void Accept()
         {
+            while (listening)
+            {
+                try
+                {
+                    Socket newClient = serverSocket.Accept();
+                    clientSockets.Add(newClient);
+                    serverConsole.AppendText("A client is connected.\n");
 
+                    Thread receiveThread = new Thread(() => Receive(newClient)); // updated
+                    receiveThread.Start();
+                }
+                catch
+                {
+                    if (terminate)
+                    {
+                        listening = false;
+                    }
+                    else
+                    {
+                        serverConsole.AppendText("The socket stopped working.\n");
+                    }
+
+                }
+            }
         }
+
+        private void Receive(Socket thisClient)
+        {
+            bool connected = true;
+            while (connected && !terminate)
+            {
+                try
+                {
+                    Byte[] buffer = new Byte[64];
+                    thisClient.Receive(buffer);
+
+                    string incomingMessage = Encoding.Default.GetString(buffer);
+                    incomingMessage = incomingMessage.Substring(0, incomingMessage.IndexOf("\0"));
+                    string username = incomingMessage;
+                    bool userExists = false;
+                    string confirmMessage = "1";
+                    string errorMessage = "0";
+                    foreach (string line in File.ReadLines(@"../../user-db.txt"))
+                    {
+                        string databaseUser = line;
+                        if (username == databaseUser)
+                        {
+                            userExists = true;
+                            break;
+                        }
+                    }
+                    if (userExists)
+                    {
+                        
+                        Byte[] buffer2 = Encoding.Default.GetBytes(confirmMessage);
+                        thisClient.Send(buffer2);
+                        
+                    }
+                    else
+                    {
+                        
+                        Byte[] buffer3 = Encoding.Default.GetBytes(errorMessage);
+                        thisClient.Send(buffer3);
+                        
+                    }
+
+                }
+                catch
+                {
+                    if (!terminate)
+                    {
+                        serverConsole.AppendText("A client has disconnected\n");
+                    }
+                    thisClient.Close();
+                    clientSockets.Remove(thisClient);
+                    connected = false;
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            listening = false;
+            terminate = true;
+            foreach (var item in clientSockets)
+            {
+                Byte[] sendError = Encoding.Default.GetBytes("The server has disconnected!");
+                item.Send(sendError);
+            }
+            Environment.Exit(0);
+        }
+
     }
 }
