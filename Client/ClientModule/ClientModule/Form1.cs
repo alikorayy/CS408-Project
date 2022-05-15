d79a460f40a2eaf6329ce424a20bf50e27dcf3b6
Mehmet Kiraz <fymmk.1903@gmail.com>
2022-05-15 17:13:09 +0300

    error message changed

--- Client/ClientModule/ClientModule/Form1.cs
+++ Client/ClientModule/ClientModule/Form1.cs
@@ -86,7 +86,7 @@ namespace ClientModule
                     if (incomingmessage == "0")
                     {
                         connected = false;
-                        client_log.AppendText("Please enter a vaild username! 0 \n");
+                        client_log.AppendText("This username is not in the database!\n");
                         Byte[] buffer_username = Encoding.Default.GetBytes(message1);
                         clientsocket.Send(buffer_username);
                         clientsocket.Close();
